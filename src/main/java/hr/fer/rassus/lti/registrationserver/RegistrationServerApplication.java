fbfa4ef6642d4c3048953ad0e88ec5ef1570ed45
jezicKarlo <karlo.jezic@gmail.com>
2021-01-06 13:31:34 +0100

    thats it

--- src/main/java/hr/fer/rassus/lti/registrationserver/RegistrationServerApplication.java
+++ src/main/java/hr/fer/rassus/lti/registrationserver/RegistrationServerApplication.java
@@ -2,12 +2,10 @@ package hr.fer.rassus.lti.registrationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class RegistrationServerApplication {
 
     public static void main(String[] args) {
